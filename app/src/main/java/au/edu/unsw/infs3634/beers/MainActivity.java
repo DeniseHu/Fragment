ca6e69a35f1dc8abc1fa00162f1404c149e57bb3
DeniseHu <dhu@outlook.com.au>
2019-03-30 00:08:10 +1100

    first commit

---
+++ app/src/main/java/au/edu/unsw/infs3634/beers/MainActivity.java
@@ -0,0 +1,29 @@
+package au.edu.unsw.infs3634.beers;
+
+import android.content.pm.ActivityInfo;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    private boolean mTwoPane;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if (findViewById(R.id.detail_container) != null) {
+            mTwoPane = true;
+        }
+
+        RecyclerView mRecyclerView = findViewById(R.id.rvList);
+        mRecyclerView.setHasFixedSize(true);
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+        RecyclerView.Adapter mAdapter = new BeerAdapter(this, Beer.getDummyBeers(), mTwoPane);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+
+}
